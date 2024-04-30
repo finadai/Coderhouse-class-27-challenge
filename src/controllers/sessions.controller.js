b01cd746d8549bf5635c246f54304ab79b408475
Fidel Nadai <fidelnadai1@outlook.es>
2024-04-30 16:37:09 -0300

    mehabiaolvidadodeestotambien

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,11 +1,35 @@
 const userDao = require('../daos/user.dao');
 
 exports.login = async (req, res) => {
-    //inicio de sesión
+    const { email, password } = req.body;
+
+    try {
+        const user = await userDao.findByEmail(email);
+        
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({ message: 'Credenciales inválidas' });
+        }
+
+        req.session.user = user;
+
+        res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    } catch (error) {
+        console.error('Error en el inicio de sesión:', error);
+        res.status(500).json({ message: 'Error en el servidor' });
+    }
 };
 
-exports.currentSession = async (req, res) => {
-    //sesión actual
+exports.logout = async (req, res) => {
+    try {
+        delete req.session.user;
+        
+        res.status(200).json({ message: 'Sesión cerrada correctamente' });
+    } catch (error) {
+        console.error('Error al cerrar sesión:', error);
+        res.status(500).json({ message: 'Error en el servidor' });
+    }
 };
+
+
 
 
--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,14 +1,26 @@
 
 exports.showRegisterPage = async (req, res) => {
-    // Renderiza la página de registro
+    res.render('register');
 };
 
 exports.showLoginPage = async (req, res) => {
-    // Renderiza la página de inicio de sesión
+    res.render('login');
 };
 
 exports.showProductsPage = async (req, res) => {
-    // Renderiza la página de productos
+    try {
+        const user = req.session.user;
+
+        if (!user) {
+            return res.redirect('/login'); // Redireccionar a la página de inicio de sesión si no hay usuario autenticado
+        }
+
+        res.render('products', { user });
+    } catch (error) {
+        console.error('Error al renderizar la página de productos:', error);
+        res.status(500).send('Error en el servidor');
+    }
 };
+
 
 
